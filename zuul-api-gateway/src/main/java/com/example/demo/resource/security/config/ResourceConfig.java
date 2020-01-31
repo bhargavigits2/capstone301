c0b72c15cf26e56df7d7e92a7dbbb0803fe396c2
bhargavigits2 <bhargavi.sce@gmail.com>
2020-01-31 07:37:12 +0000

    modified

--- zuul-api-gateway/src/main/java/com/example/demo/resource/security/config/ResourceConfig.java
+++ zuul-api-gateway/src/main/java/com/example/demo/resource/security/config/ResourceConfig.java
@@ -5,28 +5,32 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
-public class ResourceConfig extends ResourceServerConfigurerAdapter{
+@EnableResourceServer
+public class ResourceConfig extends  ResourceServerConfigurerAdapter {
 	
 	@Autowired
-	private DataSource dataSource;
-	
-	private static final String RESOURCE_ID = "resource_id";
-	
+	DataSource dataSource;
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		resources.resourceId("resource_id").tokenStore(new JdbcTokenStore(dataSource));
+		resources.resourceId("resource").tokenStore(getTokenStore());
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("auth/oauth/token").permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/auth/oauth/token").permitAll().anyRequest().authenticated();
 	}
 	
 	
+	public TokenStore getTokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
 
 }
